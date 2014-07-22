71e8b41dce5f38b844e2114d6d8d2d226e1e09f8
Anubhav410 <anubhaviam@gmail.com>
2014-07-22 19:16:14 +0530

    Programming Assignment 1 : part 3 :complete

--- corr.R
+++ corr.R
@@ -8,4 +8,50 @@ corr <- function(directory, threshold = 0) {
         ## nitrate and sulfate; the default is 0
 
         ## Return a numeric vector of correlations
-}
+		directory <- paste(directory, "/" , sep="")
+			temp <- c()
+	
+		for(num in 1:332)
+		{
+			
+			if(num < 10)
+			{
+				filename <- paste(c("00") , num , sep="")
+				filename <- paste(filename , ".csv" , sep = "")
+			}
+			else if(num > 9 & num < 100)
+			{
+				filename <- paste("0" , num , sep="")
+				filename <- paste(filename , ".csv" , sep = "")
+			}
+			else
+			{
+				filename <- paste(num , ".csv" , sep = "")
+			}
+			
+			filename <- paste(directory , filename , sep = "")
+
+			nobs <- complete(directory , num )$nobs
+
+			if(nobs > threshold)
+			{
+					file_handle <- file(filename , "rt")
+					data_t <- read.csv(file_handle)
+					x <- data_t["nitrate"]
+					y <- data_t["sulfate"]
+	
+					x_t <- x[!is.na(x) & !is.na(y)]
+					
+					y_t <- y[!is.na(x) & !is.na(y)]
+
+					
+					temp <- c(temp , cor(x_t,y_t))
+					close(file_handle)
+#					print (x)
+#					print (y)
+					}
+
+		}
+#			print (temp)
+			temp
+	}
